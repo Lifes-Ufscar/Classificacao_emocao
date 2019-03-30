d66a1d14e133b2bc55a54f3aad36a32032f1a236
Isaque Elcio <isaqueelcio@hotmail.com>
2019-03-30 15:25:34 -0300

    ECG e GSR

--- classificadorECG.py
+++ classificadorECG.py
@@ -3,16 +3,17 @@ class ClassificadorECG():
     def __init__(self, sinalECG):
         self.sinalECG = sinalECG
 
-        print("teste1", sinalECG)
-
+       
     def classificador_ecg(self):
-        print("inicio")
+        
         import pickle
+        
+            
         lda = pickle.load(open('dadosECG//lda_ecg.sav', 'rb'))
         ##Classificador
         nb_ecg = pickle.load(open('dadosECG//nb_ecg.sav', 'rb'))
-
-        print("teste2", self.sinalECG)
+      
+        
         #resultados = self.sinalGSR
         #novo_registro = np.asarray(sinalGSR)
         novo_registro = lda.transform(self.sinalECG)
--- classificadorGSR.py
+++ classificadorGSR.py
@@ -4,8 +4,7 @@ class ClassificadorGSR(object):
 
     def __init__(self, sinalGSR):
         self.sinalGSR = sinalGSR
-
-        print("teste1", sinalGSR)
+     
 
     def classificador_gsr(self):
 
@@ -14,9 +13,7 @@ class ClassificadorGSR(object):
         ##Classificador
         nb_gsr = pickle.load(open('dadosGSR//nb_gsr.sav', 'rb'))
 
-        print("teste2", self.sinalGSR)
-        #resultados = self.sinalGSR
-        #novo_registro = np.asarray(sinalGSR)
+        
         novo_registro = lda.transform(self.sinalGSR)
         resultados = nb_gsr.predict(novo_registro)
         return resultados
